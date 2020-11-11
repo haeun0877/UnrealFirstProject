95790f8427d93e99324e7b037434951840e21f2a
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2020-11-11 17:30:59 +0900

    Create enemy

--- Critter.cpp
+++ Critter.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Critter.h"
 
 // Sets default values
@@ -9,6 +8,11 @@ ACritter::ACritter()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SkeletalComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeltalComponent"));
+	SkeletalComponent->SetupAttachment(GetRootComponent());
+
+	//AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
